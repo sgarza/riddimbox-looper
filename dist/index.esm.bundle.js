993cb526cea548953dc84c0b40aedeb48b2a69fe
Sergio de la Garza <sergio@delagarza.io>
2019-09-12 10:58:50 -0500

    Initial commit

---
+++ dist/index.esm.bundle.js
@@ -0,0 +1,15 @@
+/**
+ * javascript-lib-boilerplate v0.0.2
+ * Javascript library boilerplate
+ *
+ * @author Sergio de la Garza
+ * @license MIT
+ * @preserve
+ */
+
+var sum = function sum(a, b) {
+  return a + b;
+};
+
+export { sum };
+//# sourceMappingURL=index.esm.bundle.js.map
---
+++ rollup.config.js
@@ -0,0 +1,55 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import babel from "rollup-plugin-babel";
+import builtins from "rollup-plugin-node-builtins";
+
+import {
+  version,
+  author,
+  name,
+  main,
+  license,
+  description,
+  moduleName,
+  module as moduleFile
+} from "./package.json";
+
+const banner = `\
+/**
+ * ${name} v${version}
+ * ${description}
+ *
+ * @author ${author}
+ * @license ${license}
+ * @preserve
+ */
+`;
+
+export default [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: moduleFile,
+        format: "esm",
+        sourcemap: true,
+        banner
+      },
+      {
+        file: main,
+        format: "umd",
+        sourcemap: true,
+        name: moduleName,
+        banner
+      }
+    ],
+    plugins: [
+      resolve(),
+      builtins(),
+      babel({
+        exclude: "node_modules/**"
+      }),
+      commonjs()
+    ]
+  }
+];
