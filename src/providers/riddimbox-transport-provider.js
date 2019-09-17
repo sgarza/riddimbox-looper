d67dd1b4e59f400d6d133ec506c50206c97b25e8
Sergio de la Garza <sergio@delagarza.io>
2019-09-17 11:28:02 -0500

    Fire a "loop" event on each loop

--- src/providers/riddimbox-transport-provider.js
+++ src/providers/riddimbox-transport-provider.js
@@ -1,6 +1,13 @@
 class RiddimBoxTransportProvider {
   constructor(Transport) {
     this.transport = Transport;
+    this.loopLength = 4;
+
+    this.transport.on("beat", beats => {
+      if (beats % this.loopLength === 0) {
+        this.transport.provider.emit("loop");
+      }
+    });
   }
 
   on(event, handler) {
