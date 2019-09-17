88a11bf69a73a7e96fb95582ef0b8647b3f125c6
Sergio de la Garza <sergio@delagarza.io>
2019-09-17 10:04:42 -0500

    - Add new methods to get/set the transportProvider - Add a method to setup the transport events - Add a new method to start recording

--- src/looper.js
+++ src/looper.js
@@ -1,17 +1,37 @@
 class Looper {
   static _mediaRecorderProvider = null;
+  static _transportProvider = null;
 
   static get mediaRecorderProvider() {
     Looper._throwIfMediaRecorderProvderNotSet();
     return Looper._mediaRecorderProvider;
   }
 
+  static get transportProvider() {
+    Looper._throwIfTransportProviderNotSet();
+    return this._transportProvider;
+  }
+
   static set mediaRecorderProvider(mediaRecorderProvider) {
     Looper._mediaRecorderProvider = mediaRecorderProvider;
   }
 
+  static set transportProvider(transportProvider) {
+    Looper._transportProvider = transportProvider;
+
+    Looper._setupTransportEvents();
+  }
+
   static set input(input) {
     Looper.mediaRecorderProvider.input = input;
+  }
+
+  static startRecording() {
+    Looper.mediaRecorderProvider.startRecording();
+  }
+
+  static selectCurrentLoop() {
+    Looper._throwIfTransportProviderNotSet();
   }
 
   static _throwIfMediaRecorderProvderNotSet() {
@@ -20,6 +40,18 @@ class Looper {
         "You need to set a MediaRecorderProvider first, try with ToneMediaRecorderProvider class."
       );
     }
+  }
+
+  static _throwIfTransportProviderNotSet() {
+    if (!Looper._transportProvider) {
+      throw new Error(
+        "You need to set a TransportProvider first, try with RiddimBoxTransportProvider class."
+      );
+    }
+  }
+
+  static _setupTransportEvents() {
+    this.transportProvider.on("start", Looper.startRecording);
   }
 }
 
