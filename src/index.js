5734b9ad0a2b51e97c523a0054916550ecfc3967
Sergio de la Garza <sergio@delagarza.io>
2019-09-17 10:03:18 -0500

    Export the new RiddimBoxTransportProvider class

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ export { default as Looper } from "./looper";
 export {
   default as ToneMediaRecorderProvider
 } from "./providers/tone-mediarecorder-provider";
+export {
+  default as RiddimBoxTransportProvider
+} from "./providers/riddimbox-transport-provider";
