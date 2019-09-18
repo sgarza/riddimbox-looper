84ed769692c04f81203829596dbbe4cd84338648
Sergio de la Garza <sergio@delagarza.io>
2019-09-18 10:39:17 -0500

    - Remove unused vars - Setup MediaRecorder ondataavailable handler to instantiate a Tone.Player and store the instance in the loops array

--- src/providers/tone-mediarecorder-provider.js
+++ src/providers/tone-mediarecorder-provider.js
@@ -1,14 +1,11 @@
-import {
-  MEDIA_RECORDER_INACTIVE,
-  MEDIA_RECORDER_RECORDING
-} from "../constants";
-
 class ToneMediaRecorderProvider {
   constructor(Tone, MediaRecorder) {
     this.engine = Tone;
     this.MediaRecorder = MediaRecorder;
     this.recorderStreamDestination = null;
     this.recorder = null;
+    this.selectCurrentLoop = false;
+    this.loops = [];
 
     this._setupMediaRecorder();
   }
@@ -30,6 +27,32 @@ class ToneMediaRecorderProvider {
     this.recorder = new this.MediaRecorder(
       this.recorderStreamDestination.stream
     );
+
+    this.recorder.ondataavailable = this._onDataAvailableHandler;
+  }
+
+  _onDataAvailableHandler(e) {
+    if (!this.selectCurrentLoop) return;
+
+    this.selectCurrentLoop = false;
+
+    const blob = new Blob([e.data], {
+      type: "audio/webm;; codec=opus"
+    });
+
+    const url = URL.createObjectURL(blob);
+
+    const buffer = new this.engine.Buffer(url, () => {
+      this._createBufferCallback(buffer);
+    });
+  }
+
+  _createBufferCallback(buffer) {
+    const player = new this.engine.Player(buffer);
+
+    this.loops.push({
+      player
+    });
   }
 }
 
